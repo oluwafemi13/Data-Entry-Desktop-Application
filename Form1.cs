5fb727bd225459bc6e9be8d0966338e1f41175e6
oluwafemi13 <sundayjabikem@gmail.com>
2022-09-03 23:41:45 +0100

    Designed the form

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Data_Entry_Application
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
