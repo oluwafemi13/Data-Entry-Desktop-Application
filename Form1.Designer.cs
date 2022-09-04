96d9a5e4c90ad6b5fdbef9202d546b7ad49cef6d
oluwafemi13 <sundayjabikem@gmail.com>
2022-09-04 15:20:08 +0100

    added TImer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,61 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtAddress = new System.Windows.Forms.TextBox();
-            this.txtCity = new System.Windows.Forms.TextBox();
-            this.txtState = new System.Windows.Forms.TextBox();
-            this.txtZip = new System.Windows.Forms.TextBox();
-            this.btnSave = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.grpDataEntry = new System.Windows.Forms.GroupBox();
             this.btnClear = new System.Windows.Forms.Button();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnPause = new System.Windows.Forms.Button();
-            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.txtTimer = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tickTimer = new System.Windows.Forms.Timer(this.components);
+            this.grpDataEntry.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // grpDataEntry
             // 
-            this.groupBox1.Controls.Add(this.btnClear);
-            this.groupBox1.Controls.Add(this.btnSave);
-            this.groupBox1.Controls.Add(this.txtZip);
-            this.groupBox1.Controls.Add(this.txtState);
-            this.groupBox1.Controls.Add(this.txtCity);
-            this.groupBox1.Controls.Add(this.txtAddress);
-            this.groupBox1.Controls.Add(this.txtName);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(13, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(329, 475);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Data Entry";
+            this.grpDataEntry.Controls.Add(this.btnClear);
+            this.grpDataEntry.Controls.Add(this.btnSave);
+            this.grpDataEntry.Controls.Add(this.txtZip);
+            this.grpDataEntry.Controls.Add(this.txtState);
+            this.grpDataEntry.Controls.Add(this.txtCity);
+            this.grpDataEntry.Controls.Add(this.txtAddress);
+            this.grpDataEntry.Controls.Add(this.txtName);
+            this.grpDataEntry.Controls.Add(this.label5);
+            this.grpDataEntry.Controls.Add(this.label4);
+            this.grpDataEntry.Controls.Add(this.label3);
+            this.grpDataEntry.Controls.Add(this.label2);
+            this.grpDataEntry.Controls.Add(this.label1);
+            this.grpDataEntry.Location = new System.Drawing.Point(13, 12);
+            this.grpDataEntry.Name = "grpDataEntry";
+            this.grpDataEntry.Size = new System.Drawing.Size(329, 475);
+            this.grpDataEntry.TabIndex = 0;
+            this.grpDataEntry.TabStop = false;
+            this.grpDataEntry.Text = "Data Entry";
             // 
-            // groupBox2
+            // btnClear
             // 
-            this.groupBox2.Controls.Add(this.txtTimer);
-            this.groupBox2.Controls.Add(this.btnStop);
-            this.groupBox2.Controls.Add(this.btnPause);
-            this.groupBox2.Controls.Add(this.btnStart);
-            this.groupBox2.Location = new System.Drawing.Point(372, 22);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(304, 465);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Timer";
+            this.btnClear.Location = new System.Drawing.Point(206, 416);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 38);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(89, 416);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 38);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(89, 359);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(211, 26);
+            this.txtZip.TabIndex = 9;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(89, 291);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(211, 26);
+            this.txtState.TabIndex = 8;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(89, 224);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(211, 26);
+            this.txtCity.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(89, 151);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(211, 26);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(89, 77);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(211, 26);
+            this.txtName.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 362);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Zip Code";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "State";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
             // 
             // label1
             // 
@@ -94,121 +174,18 @@
             this.label1.Text = "Name";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // groupBox2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 157);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Address";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 224);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "City";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 297);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "State";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 362);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(73, 20);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Zip Code";
-            // 
-            // txtName
-            // 
-            this.txtName.Location = new System.Drawing.Point(89, 77);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(211, 26);
-            this.txtName.TabIndex = 5;
-            // 
-            // txtAddress
-            // 
-            this.txtAddress.Location = new System.Drawing.Point(89, 151);
-            this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(211, 26);
-            this.txtAddress.TabIndex = 6;
-            // 
-            // txtCity
-            // 
-            this.txtCity.Location = new System.Drawing.Point(89, 224);
-            this.txtCity.Name = "txtCity";
-            this.txtCity.Size = new System.Drawing.Size(211, 26);
-            this.txtCity.TabIndex = 7;
-            // 
-            // txtState
-            // 
-            this.txtState.Location = new System.Drawing.Point(89, 291);
-            this.txtState.Name = "txtState";
-            this.txtState.Size = new System.Drawing.Size(211, 26);
-            this.txtState.TabIndex = 8;
-            // 
-            // txtZip
-            // 
-            this.txtZip.Location = new System.Drawing.Point(89, 359);
-            this.txtZip.Name = "txtZip";
-            this.txtZip.Size = new System.Drawing.Size(211, 26);
-            this.txtZip.TabIndex = 9;
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(89, 416);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(100, 38);
-            this.btnSave.TabIndex = 10;
-            this.btnSave.Text = "&Save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // btnClear
-            // 
-            this.btnClear.Location = new System.Drawing.Point(206, 416);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(94, 38);
-            this.btnClear.TabIndex = 11;
-            this.btnClear.Text = "&Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(96, 120);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(118, 47);
-            this.btnStart.TabIndex = 12;
-            this.btnStart.Text = "Sta&rt";
-            this.btnStart.UseVisualStyleBackColor = true;
-            // 
-            // btnPause
-            // 
-            this.btnPause.Location = new System.Drawing.Point(96, 236);
-            this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(118, 47);
-            this.btnPause.TabIndex = 13;
-            this.btnPause.Text = "&Pause";
-            this.btnPause.UseVisualStyleBackColor = true;
-            // 
-            // btnStop
-            // 
-            this.btnStop.Location = new System.Drawing.Point(96, 397);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(118, 47);
-            this.btnStop.TabIndex = 14;
-            this.btnStop.Text = "&Exit";
-            this.btnStop.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.txtTimer);
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnPause);
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Location = new System.Drawing.Point(372, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(304, 465);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Timer";
             // 
             // txtTimer
             // 
@@ -221,6 +198,42 @@
             this.txtTimer.TabIndex = 15;
             this.txtTimer.Text = "00:00:00";
             this.txtTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTimer.TextChanged += new System.EventHandler(this.txtTimer_TextChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(96, 397);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(118, 47);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(96, 236);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(118, 47);
+            this.btnPause.TabIndex = 13;
+            this.btnPause.Text = "&Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(96, 120);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(118, 47);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "Sta&rt";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tickTimer
+            // 
+            this.tickTimer.Interval = 1000;
+            this.tickTimer.Tick += new System.EventHandler(this.tickTimer_Tick);
             // 
             // DataEntryForm
             // 
@@ -228,11 +241,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(756, 545);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grpDataEntry);
             this.Name = "DataEntryForm";
             this.Text = "Data Entry Application";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.grpDataEntry.ResumeLayout(false);
+            this.grpDataEntry.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -241,7 +254,7 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox grpDataEntry;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
@@ -256,9 +269,10 @@
         private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.TextBox txtTimer;
-        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnPause;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer tickTimer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,9 @@ namespace Data_Entry_Application
 {
     public partial class DataEntryForm : Form
     {
+
+        TimeSpan timeElapsed;
+        DateTime lastElapsed;
         public DataEntryForm()
         {
             InitializeComponent();
@@ -20,6 +23,62 @@ namespace Data_Entry_Application
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+           Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+           txtAddress.Clear();
+            txtCity.Clear();
+            txtName.Clear();
+            txtState.Clear();
+            txtTimer.Clear();
+            txtZip.Clear();
+            txtName.Focus();    
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnPause.Enabled = true;
+            btnStart.Enabled = false;
+            tickTimer.Enabled = true;
+            grpDataEntry.Enabled=true;
+            txtName.Focus();
+            lastElapsed=DateTime.Now;
+
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnPause.Enabled = false;
+            grpDataEntry.Enabled = false;
+            tickTimer.Enabled=false;
+        }
+
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtTimer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tickTimer_Tick(object sender, EventArgs e)
+        {
+            timeElapsed = DateTime.Now - lastElapsed;
+            lastElapsed = DateTime.Now;
         }
     }
 }
